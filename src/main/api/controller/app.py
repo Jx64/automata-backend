2f0213e3285addb80f06f92311cf4c19ed1a01e1
Jesus Diaz <104266574+Jx64@users.noreply.github.com>
2022-10-10 15:31:30 -0500

    Update app.py

--- src/main/api/controller/app.py
+++ src/main/api/controller/app.py
@@ -1,4 +1,4 @@
-from flask import Flask, jsonify
+from flask import Flask, jsonify, request
 from flask_cors import CORS, cross_origin
 from automata.fa.nfa import NFA
 from automataDesign import Automata
@@ -7,39 +7,40 @@ app = Flask(__name__)
 CORS(app)
 
 @cross_origin
-@app.route('/validate/word=', methods=['GET'])
-def emptyWord():
-    return jsonify({"word": "",
+@app.route('/validate', methods=["GET"])
+def automataWord():
+    args = request.args.get('word')
+    if (args == ""):
+        return jsonify({"word": "",
                     "valid": True,
                     "stepwise": []})
-
-@cross_origin
-@app.route('/validate/word=<string:word>', methods=["GET"])
-def automataWord(word):
-    nonDeterministicFiniteAutomata = NFA(
-                                states={'q0', 'q1', 'q2'},
-                                input_symbols={'a', 'b', 'c'},
-                                transitions={
-                                    'q0': {'a': {'q1'}, 'b': {'q2'}, 'c': {'q2'}},
-                                    'q1': {'a': {'q1'}},
-                                    'q2': {'b': {'q2'}, 'c': {'q2'}}
-                                },
-                                initial_state='q0',
-                                final_states={'q0', 'q1', 'q2'}
-                            )   
-    getElementsInStepwiseGenerator = list(nonDeterministicFiniteAutomata.read_input_stepwise(word))
-    routeElements = []
-    for element in range(len(word)+1):
-        routeElements.append(getElementsInStepwiseGenerator[element].pop())
-
-    valid = None
-    if nonDeterministicFiniteAutomata.accepts_input(word):
-        valid = False
     else:
-        valid = True
-    validate = Automata(word, valid)
+        nonDeterministicFiniteAutomata = NFA(
+                                    states={'q0', 'q1', 'q2'},
+                                    input_symbols={'a', 'b', 'c'},
+                                    transitions={
+                                        'q0': {'a': {'q1'}, 'b': {'q2'}, 'c': {'q2'}},
+                                        'q1': {'a': {'q1'}},
+                                        'q2': {'b': {'q2'}, 'c': {'q2'}}
+                                    },
+                                    initial_state='q0',
+                                    final_states={'q0', 'q1', 'q2'}
+                                )   
+        
+        if nonDeterministicFiniteAutomata.accepts_input(args):
+            getElementsInStepwiseGenerator = list(nonDeterministicFiniteAutomata.read_input_stepwise(args))
+            routeElements = []
+            for element in range(len(args)+1):
+                routeElements.append(getElementsInStepwiseGenerator[element].pop())
 
-    return jsonify(validate.automataDesignProcess(routeElements)) 
+            newAutomata = Automata(args, True)
+                
+            return jsonify(newAutomata.automataDesignProcess(routeElements)) 
+        else:
+            return jsonify({"word": args,
+                    "valid": False,
+                    "stepwise": []})
+
 
 if __name__ == '__main__':
     app.run(debug=True, port=4000)

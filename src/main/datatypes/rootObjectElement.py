0c02455b2ee2089cd3acfa25956934e9de5da1c8
Jesus Diaz <104266574+Jx64@users.noreply.github.com>
2022-10-12 11:14:51 -0500

    Create rootObjectElement.py

---
+++ src/main/datatypes/rootObjectElement.py
@@ -0,0 +1,16 @@
+from typing import List
+from stepwise import Stepwise
+
+
+class RootObjectElement:
+    stepwise: List[Stepwise]
+    valid: bool
+    word: str
+
+    def __init__(self, stepwise: List[Stepwise], valid: bool, word: str) -> None:
+        self.stepwise = stepwise
+        self.valid = valid
+        self.word = word
+
+    def get(self):
+        return {"stepwise": self.stepwise, "valid": self.valid, "word": self.word}

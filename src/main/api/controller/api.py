7e21bd664f2346ce385189f875be9625d755b218
Jesus Diaz <104266574+Jx64@users.noreply.github.com>
2022-10-12 17:02:09 -0500

    Update api.py

--- src/main/api/controller/api.py
+++ src/main/api/controller/api.py
@@ -1,43 +1,13 @@
-import re
 from flask import Flask, jsonify, request
 from flask_cors import CORS, cross_origin
-from main.datatypes.rootObjectElement import RootObjectElement
-from main.datatypes.stepwise import Stepwise
-from main.automata.generateAutomata import Automaton
-from main.automata.automataMethods import Automata
+from main.api.apiMethods import Api
 
 app = Flask(__name__)
 CORS(app)
 
 @cross_origin
 @app.route('/validate', methods=["GET"])
-def automataWord():
-    args = request.args.get('word')
-    args.lower()
-    if args == "":
-        return jsonify({"stepwise": [], "valid": True, "word": args})
-    else:
-        if Automaton().isValid(args):
-            newAutomata = Automata(args)
-            newAutomata.stepwiseGenerator(newAutomata.nodeRoute())
-            roe = RootObjectElement(Stepwise().get(), True, args)
-            return jsonify(roe.get())
-
-        else:
-            if args[0] == 'a':
-                newArgs = re.split("[b-z]|[0-9]", args)
-                newAutomata = Automata(newArgs[0])
-                newAutomata.stepwiseGenerator(newAutomata.nodeRoute())
-                roe = RootObjectElement(Stepwise().get(), False, args)
-                return jsonify(roe.get())
-            elif args[0] == 'b' or args[0] == 'c':
-                newArgs = re.split("a|[d-z]|[0-9]", args)
-                newAutomata = Automata(newArgs[0])
-                newAutomata.stepwiseGenerator(newAutomata.nodeRoute())
-                roe = RootObjectElement(Stepwise().get(), False, args)
-                return jsonify(roe.get())
-            else:
-                return jsonify({"stepwise": [], "valid": False, "word": args})
-
-if __name__ == '__main__':
-    app.run(debug=True, port=4000)
+def get_automata():
+    queryString = request.args.get('word')
+    api = Api(queryString.lower())
+    return jsonify(api.establishRequestWithAutomata())

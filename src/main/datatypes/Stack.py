084a925193e89342a73a4a99b6e15bcf115cfeda
Jesus Diaz <104266574+Jx64@users.noreply.github.com>
2022-11-16 15:26:16 -0500

    Adding stack.py

---
+++ src/main/datatypes/Stack.py
@@ -0,0 +1,16 @@
+from typing import List
+
+class Stack:
+    push: str
+    pop: str
+    route = []
+
+    def __init__(self) -> None:
+        pass
+
+    def add(self, push, pop):
+        if type == 'NFA':
+            Stack.route.append({"push": push, "pop": pop})
+
+    def get(self):
+        return Stack.route

ee74e7c88913acb6cb7d6078ee8393461b2be61c
Jesus Diaz <104266574+Jx64@users.noreply.github.com>
2022-11-16 15:27:52 -0500

    Update apiMethods.py

--- src/main/api/apiMethods.py
+++ src/main/api/apiMethods.py
@@ -1,34 +1,49 @@
-from main.automata.automataMethods import Automata
-from main.automata.generateAutomata import Automaton
-from main.datatypes.rootObjectElement import RootObjectElement
-from main.datatypes.stepwise import Stepwise
+from automataMethods import Automata
+from generateAutomata import Automaton
+from rootObjectElement import RootObjectElement
+from stepwise import Stepwise
 import re
 
 class Api:
 
-    def __init__(self, queryString) -> None:
+    def __init__(self, queryString, type) -> None:
         self.queryString = queryString
+        self.type = type
 
     def setRejectedConnection(self):
         return "Reject connection"
 
     def establishRequestWithAutomata(self):
         if self.queryString == '':
-            return Api(self.queryString).getEmptyQueryStringJSON()
+            return Api(self.queryString, self.type).getEmptyQueryStringJSON()
         else:
-            return Api(self.queryString).getJSON()
+            return Api(self.queryString, self.type).getJSON()
         
     def getEmptyQueryStringJSON(self):
-        return {"stepwise": [], "valid": True, "word": self.queryString}
+        if self.type == 'NFA':
+            return {"stepwise": [], "valid": True, "word": self.queryString}
+        else:
+            return {{"stepwise": [], "valid": True, "word": self.queryString, 'stack': []}}
 
     def getJSON(self):
-        if Automaton().isValid(self.queryString):
-            Automata(self.queryString).stepwiseGenerator(Automata(self.queryString).nodeRoute())
-            return RootObjectElement(Stepwise().get(), Automaton().isValid(self.queryString), self.queryString).get()
-        else:
-            newQueryString = Api(self.queryString).getNewAcceptedWord()
-            Automata(newQueryString).stepwiseGenerator(Automata(newQueryString).nodeRoute())
-            return RootObjectElement(Stepwise().get(), False, self.queryString).get()
+        if self.type == 'NFA':
+            if Automaton().isValid(self.queryString, self.type):
+                Automata(self.queryString, self.type).stepwiseGenerator(Automata(self.queryString, self.type).nodeRoute())
+                return RootObjectElement(Stepwise().get(), Automaton().isValid(self.queryString, self.type), self.queryString).get()
+            else:
+                newQueryString = Api(self.queryString, self.type).getNewAcceptedWord()
+                Automata(newQueryString, self.type).stepwiseGenerator(Automata(newQueryString, self.type).nodeRoute())
+                return RootObjectElement(Stepwise().get(), False, self.queryString).get()
+        if self.type == 'NPDA':
+            if Automaton().isValid(self.queryString, self.type):
+                Automata(self.queryString, self.type).stepwiseGenerator(Automata(self.queryString, self.type).nodeRoute())
+                return RootObjectElement(Stepwise().get(), Automaton().isValid(self.queryString, self.type), self.queryString).get()
+            else:
+                newQueryString = Api(self.queryString, self.type).getNewAcceptedWord()
+                Automata(newQueryString, self.type).stepwiseGenerator(Automata(newQueryString, self.type).nodeRoute())
+                return RootObjectElement(Stepwise().get(), False, self.queryString).get()
+
+            
 
     #This functions divides the queryString and get a new one, this new one is a accepted word
     #would help for generate a stepwise for a non-accepted word

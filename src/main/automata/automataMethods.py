4bb84add6b9c434fbccf68b9e41603611ba3d799
Jesus Diaz <104266574+Jx64@users.noreply.github.com>
2022-11-16 15:27:27 -0500

    Update automataMethods.py

--- src/main/automata/automataMethods.py
+++ src/main/automata/automataMethods.py
@@ -1,15 +1,22 @@
-from main.automata.generateAutomata import Automaton
-from main.datatypes.stepwise import Stepwise
+from generateAutomata import Automaton
+from stepwise import Stepwise
 
 class Automata:
 
-    def __init__(self, word) -> None:
+    def __init__(self, word, type) -> None:
         self.word = word
+        self.type = type
 
+    def stackRoute(self):
+        getStackRoute = list(Automaton().generateAutomata(self.type).read_input_stepwise(self.word))
+        stackElements = []
+        for element in range(len(self.word)+1):
+            stackElements.append(getStackRoute[element].pop())
+        return stackElements
     #the method read_input_stepwise(input_str: Any) returns a set, then this function allows get the generator
     # elements and put them into a list
     def nodeRoute(self):
-        getElementsInGenerator = list(Automaton().generateAutomata().read_input_stepwise(self.word))
+        getElementsInGenerator = list(Automaton().generateAutomata(self.type).read_input_stepwise(self.word))
         nodeElements = []
         for element in range(len(self.word)+1):
             nodeElements.append(getElementsInGenerator[element].pop())

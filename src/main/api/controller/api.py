1d39047964287981de09e7c549ac7e8fcb40feef
Jesus Diaz <104266574+Jx64@users.noreply.github.com>
2022-11-16 15:28:36 -0500

    Update api.py

--- src/main/api/controller/api.py
+++ src/main/api/controller/api.py
@@ -1,13 +1,21 @@
 from flask import Flask, jsonify, request
 from flask_cors import CORS, cross_origin
-from main.api.apiMethods import Api
+from apiMethods import Api
+from pushdown import palabras
 
 app = Flask(__name__)
 CORS(app)
 
 @cross_origin
-@app.route('/validate', methods=["GET"])
-def get_automata():
+@app.route('/NFA/validate', methods=["GET"])
+def get_NFAautomata():
     queryString = request.args.get('word')
-    api = Api(queryString.lower())
+    api = Api(queryString.lower(), 'NFA')
+    return jsonify(api.establishRequestWithAutomata())
+
+@cross_origin
+@app.route('/NPDA/validate', methods=["GET"])
+def get_NDPAautomata():
+    queryString = request.args.get('word')
+    api = Api(queryString.lower(), 'NPDA')
     return jsonify(api.establishRequestWithAutomata())

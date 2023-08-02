75d953de75c17603c58079f4878bba23fe33881f
Jesus Diaz <104266574+Jx64@users.noreply.github.com>
2023-08-01 21:46:46 -0500

    Update apiMethods.py

--- src/main/api/apiMethods.py
+++ src/main/api/apiMethods.py
@@ -1,62 +1,38 @@
-from automataMethods import Automata
-from generateAutomata import Automaton
-from rootObjectElement import RootObjectElement
-from stepwise import Stepwise
+from main.automata.automataMethods import Automata
+from main.automata.generateAutomata import Automaton
+from main.datatypes.rootObjectElement import RootObjectElement
+from main.datatypes.stepwise import Stepwise
 import re
 
 class Api:
 
-    def __init__(self, queryString, type) -> None:
+    def __init__(self, queryString) -> None:
         self.queryString = queryString
-        self.type = type
 
     def setRejectedConnection(self):
         return "Reject connection"
 
     def establishRequestWithAutomata(self):
         if self.queryString == '':
-            return Api(self.queryString, self.type).getEmptyQueryStringJSON()
+            return Api(self.queryString).getEmptyQueryStringJSON()
         else:
-            return Api(self.queryString, self.type).getJSON()
-        
+            return Api(self.queryString).getJSON()
+
     def getEmptyQueryStringJSON(self):
-        if self.type == 'NFA':
-            return {"stepwise": [], "valid": True, "word": self.queryString}
-        else:
-            return {{"stepwise": [], "valid": True, "word": self.queryString, 'stack': []}}
+        return {"stepwise": [], "valid": True, "word": self.queryString}
 
     def getJSON(self):
-        if self.type == 'NFA':
-            if Automaton().isValid(self.queryString, self.type):
-                Automata(self.queryString, self.type).stepwiseGenerator(Automata(self.queryString, self.type).nodeRoute())
-                return RootObjectElement(Stepwise().get(), Automaton().isValid(self.queryString, self.type), self.queryString).get()
-            else:
-                newQueryString = Api(self.queryString, self.type).getNewAcceptedWord()
-                Automata(newQueryString, self.type).stepwiseGenerator(Automata(newQueryString, self.type).nodeRoute())
-                return RootObjectElement(Stepwise().get(), False, self.queryString).get()
-        if self.type == 'NPDA':
-            if Automaton().isValid(self.queryString, self.type):
-                Automata(self.queryString, self.type).stepwiseGenerator(Automata(self.queryString, self.type).nodeRoute())
-                return RootObjectElement(Stepwise().get(), Automaton().isValid(self.queryString, self.type), self.queryString).get()
-            else:
-                newQueryString = Api(self.queryString, self.type).getNewAcceptedWord()
-                Automata(newQueryString, self.type).stepwiseGenerator(Automata(newQueryString, self.type).nodeRoute())
-                return RootObjectElement(Stepwise().get(), False, self.queryString).get()
-
-            
+        if Automaton().isValid(self.queryString):
+            Automata(self.queryString).stepwiseGenerator(Automata(self.queryString).nodeRoute())
+            return RootObjectElement(Stepwise().get(), Automaton().isValid(self.queryString), self.queryString).get()
+        else:
+            newQueryString = Api(self.queryString).getNewAcceptedWord()
+            Automata(newQueryString).stepwiseGenerator(Automata(newQueryString).nodeRoute())
+            return RootObjectElement(Stepwise().get(), False, self.queryString).get()
 
     #This functions divides the queryString and get a new one, this new one is a accepted word
     #would help for generate a stepwise for a non-accepted word
-    #Example: "bbc823xcbb", the function returns the word "bbc" and generate a stepwise of this word
-    def getNewAcceptedWord(self):
-        language_a = "[b-z]|[0-9]"
-        language_b = "a|[d-z]|[0-9]"
-        if self.queryString[0] == 'a':
-            newWord = re.split(language_a, self.queryString)
-            return newWord[0]
-        
-        elif self.queryString[0] == 'b' or self.queryString == 'c':
-            newWord = re.split(language_b, self.queryString)
+	@@ -45,4 +60,4 @@ def getNewAcceptedWord(self):
             return newWord[0]
 
         else:
